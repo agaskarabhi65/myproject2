bd558ca713176bcb196d7aaa1c7d097aefb211fb
agaskarabhi65 <101781457+agaskarabhi65@users.noreply.github.com>
2022-04-02 10:02:50 +0530

    updated

---
+++ layout-1.js
@@ -0,0 +1,13 @@
+//for date under weather tab
+
+function timefunction(){
+
+    let a = new Date();
+    let b = a.getHours()+ ":" + a.getMinutes();
+    let c = b;
+
+    document.write(c);
+
+}
+
+
